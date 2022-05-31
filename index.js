fdca99587466e97104deedc5aa035f2864581a8a
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-05-31 15:10:43 +0300

    operator ++ --

---
+++ index.js
@@ -0,0 +1,27 @@
+//! Конкатенация
+
+console.log("Ed " + 26); //* String
+console.log("12.2" + 2); //* String
+
+let firstName = "Eduart";
+let lastName = "Tiutiunnyk";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName); //* String
+
+console.log("13" * 2); //* Number
+
+let number = 26;
+//! Инкремент
+number++;
+
+//! Декремент
+number--;
+
+console.log(++number); //* 27
+console.log(number++); //* 27
+console.log(number); //* 28
+
+console.log(--number); //* 27
+console.log(number--); //* 27
+console.log(number);//* 26
