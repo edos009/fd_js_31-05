4663d765ca394c6885e604fc16c64668ca7ac636
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-05-31 15:20:48 +0300

    Operators += -= *= /=

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 //! Конкатенация
+// console.log("Ed " + 26); //* String
+// console.log("12.2" + 2); //* String
 
-console.log("Ed " + 26); //* String
-console.log("12.2" + 2); //* String
+// let firstName = "Eduart";
+// let lastName = "Tiutiunnyk";
 
-let firstName = "Eduart";
-let lastName = "Tiutiunnyk";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName); //* String
 
-let fullName = firstName + " " + lastName;
-console.log(fullName); //* String
+// console.log("13" * 2); //* Number
 
-console.log("13" * 2); //* Number
-
-let number = 26;
+// let number = 26;
 //! Инкремент
-number++;
+// number++;
 
 //! Декремент
-number--;
+// number--;
 
-console.log(++number); //* 27
-console.log(number++); //* 27
-console.log(number); //* 28
+// console.log(++number); //* 27
+// console.log(number++); //* 27
+// console.log(number); //* 28
 
-console.log(--number); //* 27
-console.log(number--); //* 27
-console.log(number);//* 26
+// console.log(--number); //* 27
+// console.log(number--); //* 27
+// console.log(number);//* 26
+
+let number = 7;
+
+//! number = number * 10
+number *= 10;
+console.log(number);
+
+//! number = number / 10
+number /= 10;
+console.log(number);
+
+//! number = number + 10
+number += 10;
+console.log(number);
+
+//! number = number - 10
+number -= 10;
+console.log(number);
